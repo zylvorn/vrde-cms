6fb68c0256a36e894b50ca6cdd40fd0704db3eda
noobMaster <Randika.Alditia@xapiens.id>
2024-10-17 20:52:23 +0700

    inject fonts

--- src/components/custom/editor/index.tsx
+++ src/components/custom/editor/index.tsx
@@ -13,6 +13,26 @@ const Editor: React.FC<TProps> = ({ value, onChange, height }) => {
   const config = useMemo(() => {
     const cfg = {
       height: height || 'calc(100vh - 250px)',
+      controls: {
+        font: {
+          component: 'select',
+          list: {
+            'Source Sans Pro': 'Source Sans Pro',
+            Arial: 'Arial, sans-serif',
+            'Courier New': 'Courier New',
+            Georgia: 'Georgia',
+            Grotta: 'Grotta',
+            Lora: 'Lora',
+            'Lucida Sans Unicode': 'Lucida Sans Unicode',
+            Tahoma: 'Tahoma',
+            'Times New Roman': 'Times New Roman',
+            'Trebuchet MS': 'Trebuchet MS',
+            Helvetica: 'Helvetica',
+            Impact: 'Impact',
+            Verdana: 'Verdana',
+          },
+        },
+      },
       allowResizeY: true,
       readonly: false,
       triggerChangeEvent: true,
@@ -29,6 +49,7 @@ const Editor: React.FC<TProps> = ({ value, onChange, height }) => {
   return (
     <JoditEditor
       ref={editorRef}
+      // @ts-ignore
       config={config}
       onBlur={onChange}
       onChange={onChange}
